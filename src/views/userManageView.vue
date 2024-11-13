8a7f840c46d0dbd5123752aa96a0708b3e66d2da
CominS00n <spuckpoo@gmail.com>
2024-11-13 09:13:03 +0700

    fix: type array object users.roles.

--- src/views/userManageView.vue
+++ src/views/userManageView.vue
@@ -16,7 +16,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="item in users" :key="item.user_email">
+            <tr v-for="item in result" :key="item.user_email">
               <td>{{ item.user_id }}</td>
               <td>{{ item.user_name }}</td>
               <td>{{ item.user_username }}</td>
@@ -24,11 +24,11 @@
               <td>
                 <ul
                   v-for="subItem in item.roles"
-                  :key="subItem.role_id"
+                  :key="subItem"
                   class="md:inline-flex md:ml-2 first:ml-0"
                 >
-                  <li class="badge badge-ghost text-center ">
-                    <p>{{ subItem.role_name }}</p>
+                  <li class="badge badge-ghost text-center">
+                    <p>{{ subItem }}</p>
                   </li>
                 </ul>
               </td>
@@ -44,7 +44,7 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 
 // import type { User } from '@/types/ntType'
 import useUserApi from '@/composable/userApi'
@@ -53,7 +53,22 @@ import nt_icon from '@/components/icon/nt_icon.vue'
 
 const { getUsers, users } = useUserApi()
 
+type User = {
+  user_id: number
+  user_name: string
+  user_username: string
+  user_email: string
+  roles: string[]
+}
+const result = ref<User[]>([])
+
 onMounted(async () => {
   await getUsers()
+  result.value = users.value.map(user => ({
+    ...user,
+    roles: Array.isArray(user.roles)
+      ? user.roles.map(role => role.role_name)
+      : [],
+  }))
 })
 </script>
