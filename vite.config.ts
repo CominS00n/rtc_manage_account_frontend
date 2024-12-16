f7c5c010c5312ac4151779793de5f4df429d1166
CominS00n <spuckpoo@gmail.com>
2024-12-16 11:17:41 +0700

    config proxy

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,12 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    proxy: {
+      '/api/v2': {
+        target: 'http://192.168.127.174:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 })
