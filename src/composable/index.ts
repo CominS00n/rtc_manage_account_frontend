6e040ed023e8de550104ef293dde2d0c4cc4e2c8
CominS00n <spuckpoo@gmail.com>
2024-12-16 11:33:48 +0700

    up

--- src/composable/index.ts
+++ src/composable/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://192.168.127.174:8000/api/v2', // Change this to the correct URL if needed
+  baseURL: import.meta.env.VITE_API_BASE_URL,// Change this to the correct URL if needed
   withCredentials: true,
 })
 
