6787d2193a05cd30b8d6734cf44fd95f7cca75ed
CominS00n <spuckpoo@gmail.com>
2024-12-16 11:14:07 +0700

    fix api path

--- src/composable/index.ts
+++ src/composable/index.ts
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v2', // Change this to the correct URL if needed
+  baseURL: 'http://192.168.127.174:8000/api/v2', // Change this to the correct URL if needed
   withCredentials: true,
 })
+
+//192.168.127.174 172.17.0.1 172.18.0.1
 
 export default api
